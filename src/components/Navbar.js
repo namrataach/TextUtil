86c6e123762f1392000f12e2c19c256d276a547f
namrataach <namratachakraborty10nc@gmail.com>
2024-01-30 23:07:08 +0530

    5th commit ready for deployment for the time being

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -15,7 +15,7 @@ export default function Navbar(props) {
           <a className="nav-link active" aria-current="page" href="/">Home</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href={"/about"}>{props.about}</a>
+          <a className="nav-link"></a>
         </li>
         
         
